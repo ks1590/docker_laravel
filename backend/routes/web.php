555e6574fcb351aaff33458a4d6e2a7350358227
kensuke <35k.0101@gmail.com>
2021-10-17 22:30:31 +0900

    add:ホットペッパーAPI

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/example', function(){
+    // $url = 'https://jsonplaceholder.typicode.com/todos';
+
+    // $response = Http::get('https://api.github.com');
+    // $data = $response->json();
+    // dd($data);
+
+    $key = env('HOTPEPPER_KEY');
+    $url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key='.$key.'&name=あ&format=json';
+    
+    $response = Http::get($url);
+    $data = json_decode($response,true);
+    // $result = $data['results']['shop'];
+    $result = array_column($data['results']['shop'], 'name');
+
+    dd($result);
+});
